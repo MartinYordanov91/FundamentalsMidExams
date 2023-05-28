ae8b24b5b35f406f558194b1192b5d21ace075ee
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-05-28 22:21:52 +0300

    Update Shopping List.cs

--- 04. Programming Fundamentals Mid Exam/02. Shopping List/Shopping List.cs
+++ 04. Programming Fundamentals Mid Exam/02. Shopping List/Shopping List.cs
@@ -16,3 +16,60 @@
 // "{firstGrocery}, {secondGrocery}, … {nthGrocery}"
 // Examples
 
+using System;
+
+namespace _02._Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceryList = Console.ReadLine().Split("!", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string comandInput = Console.ReadLine();
+
+            while (comandInput != "Go Shopping!")
+            {
+                List<string> curentComands = comandInput.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (curentComands[0] == "Urgent")
+                {
+                    if (!groceryList.Contains(curentComands[1]))
+                    {
+                        groceryList.Insert(0, curentComands[1]);
+                    }
+                }
+                else if (curentComands[0] == "Unnecessary")
+                {
+                    if (groceryList.Contains(curentComands[1]))
+                    {
+                        groceryList.Remove(curentComands[1]);
+                    }
+                }
+                else if (curentComands[0] == "Correct")
+                {
+                    if (groceryList.Contains(curentComands[1]))
+                    {
+                        int oldItemIndex = groceryList.IndexOf(curentComands[1]);
+
+                        groceryList.Remove(curentComands[1]);
+                        groceryList.Insert(oldItemIndex, curentComands[2]);
+
+                    }
+                }
+                else if (curentComands[0] == "Rearrange")
+                {
+                    if (groceryList.Contains(curentComands[1]))
+                    {
+                        string item = curentComands[1];
+                        groceryList.Remove(item);
+                        groceryList.Add(item);
+
+                    }
+                }
+                comandInput = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", groceryList));
+        }
+    }
+}
